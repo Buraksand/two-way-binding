3380d178eee31e2f4cbf391e65ecccc50f5024af
burak_sandiraz <buraksand@gmail.com>
2018-08-26 18:59:00 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Two-Way Binding Practice';
+  username: string  = '';
+  isEmptyString: boolean  = true;
+
+  onClick() {
+    this.username = '';
+    this.isEmptyString = true;
+  }
+
+  onKeyEntered() {
+    if (this.username.length > 0) {
+      this.isEmptyString = false ;
+    } else {
+      this.isEmptyString = true;
+    }
+  }
 }
